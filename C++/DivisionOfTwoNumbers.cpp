80d354beca9a041c874c460a7d6c5522cb6506e9
jionbiju <jionbiju@gmail.com>
2022-11-02 08:06:48 +0530

    Division of two numbers using c++

---
+++ C++/DivisionOfTwoNumbers.cpp
@@ -0,0 +1,23 @@
+//
+// Created by Jion Biju on 11/2/2022.
+//Division of two numbers
+#include <iostream>
+
+int main()
+{
+    int a, b;
+    std::cout<<" Enter a number: ";
+    std::cin>>a;
+    std::cout<<"Enter another number: ";
+    std::cin>>b;
+    int division;
+    division=a/b;
+    std::cout<<a<<"/"<<b<<"="<<division;
+
+    return 0;
+}
+
+//=======================OUTPUT=========================
+//Enter a number:20
+//Enter another number:10
+//20/10=2
