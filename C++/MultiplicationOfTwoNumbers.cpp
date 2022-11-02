eb4dd04c9849054798b0aa69d19d161977a01bd3
jionbiju <jionbiju@gmail.com>
2022-11-02 08:13:20 +0530

    Multiplication of two numbers using c++

---
+++ C++/MultiplicationOfTwoNumbers.cpp
@@ -0,0 +1,24 @@
+//
+// Created by Jion Biju on 11/2/2022.
+//Multiplication of numbers.
+
+#include <iostream>
+
+int main()
+{
+    int a, b;
+    std::cout<<" Enter a number: ";
+    std::cin>>a;
+    std::cout<<"Enter another number: ";
+    std::cin>>b;
+    int multi;
+    multi=a*b;
+    std::cout<<a<<"*"<<b<<"="<<multi;
+
+    return 0;
+}
+
+//=================OUTPUT======================
+//Enter a number:2
+//Enter another number:25
+//2*25=50
