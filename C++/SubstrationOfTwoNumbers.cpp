4698d040a7c6ef8834d545b6aba23a9babd2bdeb
jionbiju <jionbiju@gmail.com>
2022-11-02 09:00:19 +0530

    Substration of two numbers using c++

---
+++ C++/SubstrationOfTwoNumbers.cpp
@@ -0,0 +1,22 @@
+//
+// Created by Jion Biju on 11/2/2022.
+//Substration of two number.
+#include <iostream>
+
+int main() {
+    int a, b;
+    std::cout << " Enter a number: ";
+    std::cin >> a;
+    std::cout << "Enter another number: ";
+    std::cin >> b;
+    int substraton;
+    substraton= a-b;
+    std::cout << a << "-" << b << "=" <<substraton;
+
+    return 0;
+}
+
+//=========================OUTPUT=====================
+//Enter a number:40
+//Enter another number:50
+//40-50=-10
