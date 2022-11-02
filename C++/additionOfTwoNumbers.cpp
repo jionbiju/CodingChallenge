87a16f4261ebb629e53695a56a02501daf6918ea
jionbiju <jionbiju@gmail.com>
2022-11-02 07:29:46 +0530

    Adition of two numbers using c++

---
+++ C++/additionOfTwoNumbers.cpp
@@ -0,0 +1,22 @@
+//
+// Created by Jion Biju on 11/2/2022.
+//Addition of two numbers.
+#include <iostream>
+
+int main()
+{
+    int a, b;
+    std::cout<<" Enter a number: ";
+    std::cin>>a;
+    std::cout<<"Enter another number: ";
+    std::cin>>b;
+    int sum;
+    sum=a+b;
+    std::cout<<"Sum"<<"="<<a<<"+"<<b<<"="<<sum;
+    return 0;
+}
+
+//=========================OUTPUT==========================
+//Enter a number:5
+//Enter another number:9
+//Sum=5+9=14
