4c1f7b8cc15c642e21bc1e89f183b4cf749efe76
jionbiju <jionbiju@gmail.com>
2022-11-16 21:36:08 +0530

    Array example 2 using c++

---
+++ C++/ArrayExample2.cpp
@@ -0,0 +1,34 @@
+//
+// Created by Jion Biju on 11/16/2022.
+//Array example 2
+#include <iostream>
+
+int main()
+{
+    int n;
+    std::cout<<"Enter the limit: ";
+    std::cin>>n;
+    char letter[n];
+    for (char i=0;i<n;i++) {
+        std::cout<<"Enter a letter: ";
+        std::cin>>letter[i];
+    }
+    for (char i=0;i<n;i++) {
+        for (char j=0;j<i+1;j++) {
+            std::cout<<"\t"<<letter[j];
+        }
+        std::cout<<std::endl;
+    }
+    return 0;
+}
+
+//===============OUTPUT===================
+//Enter the limit:4
+//Enter a letter:J
+//Enter a letter:I
+//Enter a letter:O
+//Enter a letter:N
+//        J
+//        J       I
+//        J       I       O
+//        J       I       O       N
