6a77564b26be56f09e83da95d574647cd543c49b
jionbiju <jionbiju@gmail.com>
2022-11-30 22:00:59 +0530

    To find  String length using c++

---
+++ C++/LengthOfTheString.cpp
@@ -0,0 +1,18 @@
+//
+// Created by Jion Biju on 11/30/2022.
+//To find the string length.
+#include <iostream>
+
+int main()
+{
+    std::string str;
+    std::cout<<"Enter a word: "<<std::endl;
+    std::cin>>str;
+    std::cout<<"String length= "<<str.length();
+    return 0;
+}
+
+//===========OUTPUT==============
+//Enter a word:
+//c++program
+//String length= 10
