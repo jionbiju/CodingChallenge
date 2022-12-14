761e386272fc8abcb1cda79e388972311ad3ce6f
jionbiju <jionbiju@gmail.com>
2022-12-14 22:42:07 +0530

    Array using fuction with argument without return  using c++

--- C++/ArrayUsingFuctionWithArgumentWithoutReturn.cpp
+++ C++/ArrayUsingFuctionWithArgumentWithoutReturn.cpp
@@ -2,23 +2,23 @@
 // Created by Jion Biju on 12/14/2022.
 //Array using fuction with argument and without return value.
 #include <iostream>
-void array(int);
+void display(int [],int );
 int main()
 {
     int n;
     std::cout<<"Enter array size: ";
     std::cin>>n;
-    array(n);
-    return 0;
-}
-void array(int a){
-    int arr[a];
+    int arr[n];
     std::cout<<"Enter array elements:"<<std::endl;
-    for (int i=0;i<a;i++) {
+    for (int i=0;i<n;i++) {
         std::cin>>arr[i];
     }
+    display(arr,n);
+    return 0;
+}
+void display(int arr[],int n){
     std::cout<<"Array elements are:"<<std::endl;
-    for (int i=0;i<a;i++) {
+    for (int i=0;i<n;i++) {
         std::cout<<arr[i]<<std::endl;
     }
 }
