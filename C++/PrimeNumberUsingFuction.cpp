4fd1327c9529e0f77afbb7c48a4ed0c77ea7e8ac
jionbiju <jionbiju@gmail.com>
2023-01-06 22:40:57 +0530

    Prime number using fuction using c++

--- C++/PrimeNumberUsingFuction.cpp
+++ C++/PrimeNumberUsingFuction.cpp
@@ -1,14 +1,15 @@
 //
 // Created by Jion Biju on 1/6/2023.
-/*Implement using functions to check whether the given number is prime and
-display appropriate messages (No built in Math Function).*/
+
+//Implement using functions to check whether the given number is prime and
+//display appropriate messages (No built in Math Function).
 
 #include <iostream>
 int checkprime(int);
 int main()
 {
     int n;
-    std::cout<<"Enter a positive integer:"<<std::endl;
+    std::cout<<"Enter a integer:"<<std::endl;
     std::cin>>n;
     if (checkprime(n)){
         std::cout<<n<<" is prime number.";
@@ -21,7 +22,7 @@ int main()
 
 int checkprime(int n){
     bool prime= true;
-    if (n==0||n==1){
+    if (n==0||n==1 || n<0){
         prime= false;
     }
     for (int i=2;i<=n/2;i++) {
