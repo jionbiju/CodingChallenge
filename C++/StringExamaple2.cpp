bdee50b87e52a7683bee8b13250005f7110856f4
jionbiju <jionbiju@gmail.com>
2023-06-24 15:28:30 +0530

    Hello World in  Java

---
+++ C++/StringExamaple2.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Jion Biju on 6/6/2023.
+//
+#include <iostream>
+
+int main(){
+    std::string name;
+    std::cout<<"Enter a name ";
+    std::cin>>name;
+    std::cout<<name;
+    return 0;
+}
