11b1b039f4265b669c81a98f7d9e5a10332e12af
jionbiju <jionbiju@gmail.com>
2023-07-03 22:24:58 +0530

    Calculator project using method 2 in  HTML

--- Web Design Challenge/Calculator/script.js
+++ Web Design Challenge/Calculator/script.js
@@ -1,7 +1,3 @@
-var operator
-var oldValue
-var newValue
-var result
 function buttonclick(val){
 
     document.getElementById("screen").value+=val
@@ -10,44 +6,8 @@ function buttonclick(val){
 function clearDisplay(){
     document.getElementById("screen").value=""
 }
-function plusButton(){
-    oldValue=parseInt(document.getElementById("screen").value)
-    document.getElementById("screen").value=""
-    operator='+'
-}
-function minusButton(){
-    oldValue=document.getElementById("screen").value
-    document.getElementById("screen").value=""
-    operator='-'
-}
-function multiButton(){
-    oldValue=document.getElementById("screen").value
-    document.getElementById("screen").value=""
-    operator='*'
-}
-function divisionButton(){
-    oldValue=document.getElementById("screen").value
-    document.getElementById("screen").value=""
-    operator='/'
-}
 function equalButton(){
-    newValue=parseInt(document.getElementById("screen").value)
-    document.getElementById("screen").value=""
-    result
-    if(operator =='+'){
-        result=oldValue + newValue
-        document.getElementById("screen").value=result;
-    }
-    else if(operator=='-'){
-        result=oldValue - newValue
-        document.getElementById("screen").value=result;
-    }
-    else if(operator=='*'){
-        result=oldValue * newValue
-        document.getElementById("screen").value=result;
-    }
-    else if(operator=='/'){
-        result=oldValue / newValue
-        document.getElementById("screen").value=result;
-    }
+    var text=document.getElementById("screen").value
+    var result=eval(text)
+    document.getElementById("screen").value=result
 }
