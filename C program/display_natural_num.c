921870a3f0d50e9fbdf5425e4a49f87ee3fd3f90
jionbiju <jionbiju@gmail.com>
2024-02-04 11:01:46 +0530

    display n natural num using while loop

---
+++ C program/display_natural_num.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void main(){
+    int n;
+    printf("Enter a limit: ");
+    scanf("%d",&n);
+    int i=0;
+    while (i<=n)
+    {
+        printf("%d\n",i);
+        i++;
+    }
+    
+}
