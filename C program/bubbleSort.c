005261c7d9a8a37ab257a4094e0d93ad869e949f
jionbiju <jionbiju@gmail.com>
2024-03-11 17:44:00 +0530

    bubble sort

---
+++ C program/bubbleSort.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+void main()
+{
+    int i,j,n,temp,flag;
+    printf("Enter size of the array:\n");
+    scanf("%d",&n);
+    int arr[n];
+    printf("Enter array elements:\n");
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    printf("Array elements are:\n");
+    for(i=0;i<n;i++)
+    {
+        printf("%d\n",arr[i]);
+    }
+    for(i=0;i<n-1;i++)
+    {
+        flag=0;
+        for(j=0;j<n-1-i;j++)
+        {
+            if(arr[j]>arr[j+1])
+            {
+                temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+                flag=1;
+            }
+        }
+        if(flag==0)
+        break;
+    }
+    printf("Sorted array is:\n");
+    for(j=0;j<n;j++)
+    {
+        printf("%d\n",arr[j]);
+    }
+}
