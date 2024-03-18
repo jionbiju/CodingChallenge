77ba2aaf93d1b25bfff78df0d3196b5a17e7c930
jionbiju <jionbiju@gmail.com>
2024-03-18 18:45:39 +0530

    updated

--- C program/reverseOfString.c
+++ C program/reverseOfString.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 void main()
 {
-    char s[100];
+    char,temp s[100];
     int len = 0;
     printf("Enter a string:");
     scanf("%s", s);
-    for (int i = 0; s[i] != 0; i++)
+    for (int i = 0; s[i] != '\0'; i++)
     {
         len++;
     }
     printf("String length is %d\n", len);
     for (int j = 0; j < len / 2; j++)
     {
-        char temp = s[j];
+        temp = s[j];
         s[j] = s[len - 1 - j];
         s[len - 1 - j] = temp;
     }
