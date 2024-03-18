233ed817612d201ac2c0805c6d1d2be561003f2c
jionbiju <jionbiju@gmail.com>
2024-03-18 18:34:52 +0530

    reverse  of a string without string fuctions

---
+++ C program/reverseOfString.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+void main()
+{
+    char s[100];
+    int len = 0;
+    printf("Enter a string:");
+    scanf("%s", s);
+    for (int i = 0; s[i] != 0; i++)
+    {
+        len++;
+    }
+    printf("String length is %d\n", len);
+    for (int j = 0; j < len / 2; j++)
+    {
+        char temp = s[j];
+        s[j] = s[len - 1 - j];
+        s[len - 1 - j] = temp;
+    }
+    printf("Reverse of string is %s",s);
+}
+
+/*
+OUTPUT
+Enter a string:hai
+String length is 3
+Reverse of string is iah
+*/
