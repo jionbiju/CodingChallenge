0bfc9fca2f8054ba2e402fe2b583ee7455b2dae9
jionbiju <jionbiju@gmail.com>
2024-04-05 23:42:08 +0530

    array sum

---
+++ C program/sumOfArray.c
@@ -0,0 +1,34 @@
+//sum and average of elements of an array
+#include <stdio.h>
+void main()
+{
+    int i,n=0;
+    float sum=0,avg;
+    printf("Enter the size of the array:\n");
+    scanf("%d",&n);
+    int arr[n];
+    printf("Enter the elements:\n");
+    for(i=0; i<n; i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0; i<n; i++)
+    {
+        sum=sum+arr[i];
+    }
+    printf("Sum of elements=%f\n",sum);
+    avg=sum/n;
+    printf("Averge=%f\n",avg);
+}
+/*
+OUTPUT
+Enter the size of the array:
+5
+Enter the elements:
+1
+2
+3
+4
+5
+Sum of elements=15.00
+*/ 
