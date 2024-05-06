964bdc31f21151999c72e10db22849ea08326245
jionbiju <jionbiju@gmail.com>
2024-05-06 22:44:21 +0530

    read a file and displayed it

---
+++ C program/fileExp1.c
@@ -0,0 +1,22 @@
+/*Write a C program to read a text file character 
+by character and display its content on the screen.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+void main(){
+    char ch;
+    FILE *fp=NULL;
+    fp=fopen("fileExample.txt","r");
+    if(fp==NULL){
+        printf("Eror\n");
+        exit(1);
+    }
+    while(!feof(fp)){
+        ch=fgetc(fp);
+        printf("%c",ch);
+    }
+}
+
+//Output
+//hello World 
