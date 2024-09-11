3377e46d91d51c4805a73cd3f803564750e7df95
jionbiju <jionbiju@gmail.com>
2024-09-11 20:13:01 +0530

    backend added

--- React/Todo/Backend/index.js
+++ React/Todo/Backend/index.js
@@ -1,2 +1,49 @@
 const express = require("express");
-const app = express;
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
+const port = 4000;
+const path = require("path");
+const {error} = require("console");
+const TodoModel = require('./Models/TodoModel')
+
+//Cors 
+app.use(cors({ origin: true, credentials: true }));
+app.use(express.json());
+
+//API Creation
+app.get("/",(req,res)=>{
+    res.send("Express App is Running")
+})
+
+//MongoDB connection
+
+mongoose.connect("mongodb+srv://jionbiju:670633@cluster0.wazkg.mongodb.net/Todo")
+.then(()=>{
+    app.listen(port,() => {
+        console.log("DB connected successfully and server is running in the port "+port);
+    })
+})
+.catch((err)=>console.log("Mango DB connection error "+err));
+
+//API for created for add task to DB
+app.post('/add', (req, res) => {
+    const todo = req.body.todo;
+  
+    TodoModel.create({ task: todo })
+      .then((createdTodo) => {
+        res.status(200).json(createdTodo); 
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).json({ error: 'Failed to create task' }); 
+      });
+  });
+
+  //API to Delete task
+  app.delete('/delete/:id',(req,res)=>{
+    const {id} = req.params;
+    TodoModel.findByIdAndDelete(id)
+    .then(()=>res.status(200).json({ message: 'Task deleted successfully' }))
+    .catch((error)=>console.log(error))
+  })
--- React/Todo/Frontend/src/App.js
+++ React/Todo/Frontend/src/App.js
@@ -4,16 +4,26 @@ import { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {  faCheckSquare } from '@fortawesome/free-solid-svg-icons'
 import { faTrash } from '@fortawesome/free-solid-svg-icons'
-import { v4 as uuidv4 } from "uuid";
+import axios from 'axios';
 
 function App() {
   const [todos,setTodos]=useState([])
   const [todo,setTodo]=useState('')
-  const addTodo=(e)=>{
-    setTodos([...todos,{id: uuidv4(),text:todo, status:false}])
-    setTodo("")
-    e.preventDefault()  
-  }
+  
+  const addTodo = (e) => {
+    e.preventDefault();
+    if (todo.trim() === '') return;
+    axios.post('http://localhost:4000/add', { todo })
+      .then((response) => {
+        const newTodo = { id: response.data._id, text: response.data.task, status: false };
+        setTodos([...todos, newTodo]);
+        console.log('Task added:', response.data);
+      })
+      .catch((err) => console.log('Error adding task:', err));
+  
+    setTodo("");
+  };
+
   const toggleComplete=(id)=>{
     setTodos(
       todos.map((todo) =>
@@ -23,6 +33,9 @@ function App() {
   }
   const deleteTodo=(id)=>{
     setTodos(todos.filter((todo)=> todo.id!==id))
+    axios.delete('http://localhost:4000/delete/'+id)
+    .then(()=>console.log('Task Deleted successfully'))
+    .catch((error)=>console.log('Error in sending id for deletion',error))
   }
   return (
     <div className="app">
@@ -35,7 +48,7 @@ function App() {
       <form className='todo-form' onSubmit={addTodo}>
         <input className='todo-input' value={todo} onChange={(e)=>setTodo(e.target.value)}
         type='text' placeholder='What is the task today?' />
-        <button className='todo-btn' type='submit'>Add Task</button>
+        <button  className='todo-btn' type='submit'>Add Task</button>
       </form>
       <div>
       {
