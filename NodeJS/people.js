db97f28fecb3cdb27d7cea065318cbab482e3f4f
jionbiju <jionbiju@gmail.com>
2025-03-23 21:21:37 +0530

    learning node

---
+++ NodeJS/people.js
@@ -0,0 +1,6 @@
+const people = ['h','b','c','d'];
+const ages = [20,45,54,5423];
+
+module.exports ={
+    people,ages
+};
---
+++ NodeJS/test.js
@@ -0,0 +1,22 @@
+//const Pname = 'jion';
+//console.log(Pname);
+
+/*const greet = (name) => {
+    console.log("Hello",name);
+}  
+
+greet("messi");
+
+const greet1 = (name) =>{
+    console.log(`Hello ,${name}`);
+}
+//console.log(global);
+setTimeout(()=>{
+    console.log('Time out in 4s');
+    clearInterval(int);
+},4000)
+
+const int = setInterval(() => {
+    console.log('In the interval');
+},1000)*/
+console.log(__filename);
