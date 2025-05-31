2ba5fa32653841e2803b93567667f8476f428751
jionbiju <jionbiju@gmail.com>
2025-05-31 10:53:02 +0530

    testing express

---
+++ NodeJS/app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+
+//Listen for request
+
+app.listen(3000);
+
+app.get('/', (req,res) => {
+    //res.send('<p>Home Page</p>');
+    res.sendFile('./views/index.html',{root:__dirname});
+});
+
+app.get('/about', (req,res) => {
+    res.sendFile('./views/about.html',{root:__dirname});
+    //res.send('<p>About Page</p>');
+});
+
+//Redirect
+app.get('/about-us', (req,res) => {
+    res.redirect('/about');
+});
+
+//404 error
+app.use((req,res) => {
+    res.status(404).sendFile('./views/404.html',{root:__dirname});
+})
