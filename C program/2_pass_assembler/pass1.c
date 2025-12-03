3db6e29a7304cf03f7f0917bd4e3c9c5646e9126
jionbiju <jionbiju@gmail.com>
2025-12-03 17:45:14 +0530

    pass1 of one pass assembler added

--- C program/2_pass_assembler/pass1.c
+++ C program/2_pass_assembler/pass1.c
@@ -2,103 +2,140 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main() {
-    char label[20], opcode[20], operand[20], sym[20], code[20], mnemonic[20];
-    char line[100], objcode[20];
-    int locctr, start, length, addr;
+void main()
+{
+    char label[20], opcode[20], operand[20], code[20], mnemonic[20];
+    char line[100];
+    int locctr, length, start;
     FILE *fp1, *fp2, *fp3, *fp4;
-
-    fp1 = fopen("interf.txt", "r");   // intermediate file
-    fp2 = fopen("symtab.txt", "r");   // symbol table
-    fp3 = fopen("optab.txt", "r");    // operation code table
-    fp4 = fopen("objcode.txt", "w");  // output object code
-
-    if (!fp1 || !fp2 || !fp3 || !fp4) {
-        printf("Error opening files.\n");
-        return 1;
-    }
-
-    // read first line
+    fp1 = fopen("input.txt", "r");
+    fp2 = fopen("optab.txt", "r");
+    fp3 = fopen("symtab.txt", "w");
+    fp4 = fopen("interf.txt", "w");
     fgets(line, sizeof(line), fp1);
     if (line[0] == ' ' || line[0] == '\t')
-        sscanf(line, "%s %s", opcode, operand);
+    {
+        strcpy(label, "");
+        sscanf(line, "%s%s", opcode, operand);
+    }
     else
-        sscanf(line, "%s %s %s", label, opcode, operand);
+    {
 
-    if (strcmp(opcode, "START") == 0) {
+        sscanf(line, "%s%s%s", label, opcode, operand);
+    }
+    if (strcmp(opcode, "START") == 0)
+    {
         start = (int)strtol(operand, NULL, 16);
         locctr = start;
-        fprintf(fp4, "H^%s^%06X^", label, start);
+        fprintf(fp4, "\t\t%-10s%-10s%-10s\n", label, opcode, operand);
         fgets(line, sizeof(line), fp1);
         if (line[0] == ' ' || line[0] == '\t')
-            sscanf(line, "%s %s", opcode, operand);
+        {
+            strcpy(label, "");
+            sscanf(line, "%s%s", opcode, operand);
+        }
         else
-            sscanf(line, "%s %s %s", label, opcode, operand);
-    } else {
+        {
+            sscanf(line, "%s%s%s", label, opcode, operand);
+        }
+    }
+    else
+    {
         locctr = 0;
     }
-
-    while (strcmp(opcode, "END") != 0) {
-        // search optab
-        int found = 0;
-        rewind(fp3);
-        while (fscanf(fp3, "%s %s", code, mnemonic) != EOF) {
-            if (strcmp(opcode, code) == 0) {
-                found = 1;
-                // find address of operand in symtab
-                rewind(fp2);
-                while (fscanf(fp2, "%s %X", sym, &addr) != EOF) {
-                    if (strcmp(sym, operand) == 0) {
-                        sprintf(objcode, "%s%04X", mnemonic, addr);
-                        fprintf(fp4, "%s^", objcode);
-                        break;
-                    }
-                }
+    while (strcmp(opcode, "END") != 0)
+    {
+        fprintf(fp4, "%04X\t", locctr);
+        if (strlen(label) > 0)
+        {
+            fprintf(fp3, "%-10s%04X\n", label, locctr);
+        }
+        rewind(fp2);
+        while (fscanf(fp2, "%s%s", code, mnemonic) != EOF)
+        {
+            if (strcmp(opcode, code) == 0)
+            {
                 locctr += 3;
                 break;
             }
         }
-
-        if (!found) {
-            if (strcmp(opcode, "WORD") == 0) {
-                sprintf(objcode, "%06X", atoi(operand));
-                fprintf(fp4, "%s^", objcode);
-                locctr += 3;
-            } else if (strcmp(opcode, "BYTE") == 0) {
-                int len = strlen(operand);
-                if (operand[0] == 'C') {
-                    for (int i = 2; i < len - 1; i++) {
-                        sprintf(objcode, "%02X", operand[i]);
-                        fprintf(fp4, "%s", objcode);
-                    }
-                } else if (operand[0] == 'X') {
-                    for (int i = 2; i < len - 1; i++)
-                        fprintf(fp4, "%c", operand[i]);
-                }
-                fprintf(fp4, "^");
-                locctr += (operand[0] == 'C') ? len - 3 : (len - 3) / 2;
-            } else if (strcmp(opcode, "RESW") == 0) {
-                locctr += 3 * atoi(operand);
-            } else if (strcmp(opcode, "RESB") == 0) {
-                locctr += atoi(operand);
+        if (strcmp(opcode, "WORD") == 0)
+        {
+            locctr += 3;
+        }
+        else if (strcmp(opcode, "RESW") == 0)
+        {
+            locctr += 3 * (atoi(operand));
+        }
+        else if (strcmp(opcode, "RESB") == 0)
+        {
+            locctr += atoi(operand);
+        }
+        else if (strcmp(opcode, "BYTE") == 0)
+        {
+            if (operand[0] == 'C' || operand[0] == 'c')
+            {
+                locctr += strlen(operand) - 3;
+            }
+            else if (operand[0] == 'X' || operand[0] == 'x')
+            {
+                locctr += (strlen(operand) - 3) / 2;
             }
         }
-
-        // read next line
-        if (!fgets(line, sizeof(line), fp1)) break;
+        fprintf(fp4, "%-10s%-10s%-10s\n", label, opcode, operand);
+        fgets(line, sizeof(line), fp1);
         if (line[0] == ' ' || line[0] == '\t')
-            sscanf(line, "%s %s", opcode, operand);
+        {
+            strcpy(label, "");
+            sscanf(line, "%s%s", opcode, operand);
+        }
         else
-            sscanf(line, "%s %s %s", label, opcode, operand);
+        {
+            sscanf(line, "%s%s%s", label, opcode, operand);
+        }
     }
-
-    fprintf(fp4, "\nE^%06X\n", start);
-
+    fprintf(fp4, "%04X\t%-10s%-10s%-10s\n", locctr, label, opcode, operand);
+    length = locctr - start;
+    printf("Program Length = %04X", length);
     fclose(fp1);
     fclose(fp2);
     fclose(fp3);
     fclose(fp4);
-
-    printf("Pass 2 completed. Object code generated in objcode.txt\n");
-    return 0;
 }
+
+/*
+OUTPUT
+Program Length = 001A
+
+
+input.txt
+        START   2000
+        LDA     FIVE
+        STA     ALPHA
+        LDCH    CHARZ
+        STCH    C1
+ALPHA   RESW    2
+FIVE    WORD    5
+CHARZ   BYTE    C'Zard'
+C1      RESB    1
+        END
+
+optab.txt
+LDA     03
+STCH    57
+STA     0F
+LDCH    53
+
+interf.txt
+		          START     2000      
+2000	          LDA       FIVE      
+2003	          STA       ALPHA     
+2006	          LDCH      CHARZ     
+2009	          STCH      C1        
+200C	ALPHA     RESW      2         
+2012	FIVE      WORD      5         
+2015	CHARZ     BYTE      C'Zard'   
+2019	C1        RESB      1         
+201A	          END       1         
+
+*/
