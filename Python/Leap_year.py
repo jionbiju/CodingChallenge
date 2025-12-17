01b66a5c2b79dcc9a0510d7ca175d970f89c0c1c
jionbiju <jionbiju@gmail.com>
2025-12-17 18:19:30 +0530

    pattern printing

---
+++ Python/Leap_year.py
@@ -0,0 +1,19 @@
+year = int(input("Enter a year:"))
+if year%4 == 0:
+    if year%100 == 0:
+        if year % 400 == 0:
+            print(year," is leap year.")
+        else:
+            print(year, " is not leap year.")
+    else:
+        print(year, " is leap year.")
+else:
+    print(year, " is not leap year.")
+"""
+OUTPUT
+Enter a year:1900
+1900  is not leap year.
+
+Enter a year:2024
+2024  is leap year.
+"""
