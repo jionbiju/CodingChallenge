d31e8b320d45216675644b1207a911998221267d
jionbiju <jionbiju@gmail.com>
2026-06-01 18:07:29 +0530

    stack implemented using array

---
+++ Data_structures/src/arrays/StackArray.java
@@ -0,0 +1,55 @@
+package arrays;
+
+public class StackArray {
+    int size = 100;
+    int [] stack = new int[size];
+    int top = -1;
+
+    void push(int data){
+        if (top == size-1){
+            System.out.println("stack is full");
+            return;
+        }
+        stack[++top] = data;
+    }
+
+    int pop(){
+        if (top == -1){
+            System.out.println("Stack is empty");
+            return -1;
+        }
+        return stack[top--];
+    }
+    int peek(){
+        return stack[top];
+    }
+
+    boolean isEmpty(){
+        if (top == -1){
+            return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        StackArray stackArray = new StackArray();
+        stackArray.push(1);
+        stackArray.push(2);
+        stackArray.push(3);
+        System.out.println(stackArray.pop());
+        System.out.println(stackArray.pop());
+        System.out.println(stackArray.peek());
+        System.out.println(stackArray.isEmpty());
+        System.out.println( stackArray.pop());
+        stackArray.pop();
+    }
+}
+/*
+OUTPUT
+3
+2
+1
+false
+1
+Stack is empty
+ */
