251bd75c27a97d2690e3d8d14e63e7205691aebf
jionbiju <jionbiju@gmail.com>
2026-06-16 22:24:10 +0530

    output added

--- Data_structures/src/arrays/LinearSearch.java
+++ Data_structures/src/arrays/LinearSearch.java
@@ -1,5 +1,7 @@
 package arrays;
 
+import java.util.Scanner;
+
 class LSearch{
     public static int find(int [] arr,int target){
         for(int i=0;i<arr.length ; i++){
@@ -12,8 +14,31 @@ class LSearch{
 }
 public class LinearSearch {
     public static void main(String[] args) {
-        int [] arr = {4,5,672,673,87,1,7,9};
-        int target = 673;
-        System.out.println(LSearch.find(arr,target));
+        Scanner sc = new Scanner(System.in);
+        int n;
+        System.out.println("Enter the array size:");
+        n = sc.nextInt();
+        int [] arr = new int[n];
+        System.out.println("Enter " + n+" array elements:");
+        for (int i=0; i<n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int target;
+        System.out.println("Enter the element to be searched:");
+        target = sc.nextInt();
+        System.out.println("Index:"+LSearch.find(arr,target));
     }
 }
+/*
+OUTPUT
+Enter the array size:
+6
+Enter 6 array elements:
+78 45 984 4 22 369
+Enter the element to be searched:
+4
+Index:3
+
+Process finished with exit code 0
+
+ */
