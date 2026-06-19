3988b17f4aa808da9ad19fb9115a0261abdca83c
jionbiju <jionbiju@gmail.com>
2026-06-19 23:14:51 +0530

    insertion sort implemented

---
+++ Data_structures/src/arrays/InsertionSort.java
@@ -0,0 +1,25 @@
+package arrays;
+
+import java.util.Arrays;
+
+public class InsertionSort {
+    public static void sort(int [] arr){
+        for (int i=1; i<arr.length; i++){
+            int temp = arr[i];
+            int j;
+            for (j=i-1; j>=0 && arr[j] > temp; j--){
+                arr[j+1] = arr[j];
+            }
+            arr[j+1] =temp;
+        }
+    }
+    public static void main(String[] args) {
+        int [] arr = {5,8,1,345,7646,77,45};
+        sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
+/*
+OUTPUT
+[1, 5, 8, 45, 77]
+ */
