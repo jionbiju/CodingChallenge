d10b0777ff000cf636db5cd348c3034991d2d32b
jionbiju <jionbiju@gmail.com>
2026-08-14 20:19:35 +0530

    recursive parser

---
+++ C program/parser.c
@@ -0,0 +1,93 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+char input[100];
+int pos = 0;
+
+/* Function declarations */
+void E();
+void Eprime();
+void T();
+void Tprime();
+void F();
+
+/* E -> T E' */
+void E()
+{
+    T();
+    Eprime();
+}
+
+/* E' -> + T E' | epsilon */
+void Eprime()
+{
+    if (input[pos] == '+')
+    {
+        pos++;
+        T();
+        Eprime();
+    }
+}
+
+/* T -> F T' */
+void T()
+{
+    F();
+    Tprime();
+}
+
+/* T' -> * F T' | epsilon */
+void Tprime()
+{
+    if (input[pos] == '*')
+    {
+        pos++;
+        F();
+        Tprime();
+    }
+}
+
+/* F -> (E) | id */
+void F()
+{
+    if (isalpha(input[pos]))
+    {
+        pos++;
+    }
+    else if (input[pos] == '(')
+    {
+        pos++;
+        E();
+
+        if (input[pos] == ')')
+        {
+            pos++;
+        }
+        else
+        {
+            printf("Error: Missing ')'\n");
+            exit(1);
+        }
+    }
+    else
+    {
+        printf("Error: Invalid expression\n");
+        exit(1);
+    }
+}
+
+int main()
+{
+    printf("Enter the expression: ");
+    scanf("%s", input);
+
+    E();
+
+    if (input[pos] == '\0')
+        printf("Expression is successfully parsed.\n");
+    else
+        printf("Error: Invalid expression.\n");
+
+    return 0;
+}
